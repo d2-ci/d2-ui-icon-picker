e16ca1b0097f818527b6aadfc255b5733745e9f3
joakimia <joakimamundsen91@gmail.com>
2019-03-04 08:48:21 +0000

    33c3e35 add overflow to avatarBoxContent div

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _IconPicker = require('./IconPicker.component');
+
+var _IconPicker2 = _interopRequireDefault(_IconPicker);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _IconPicker2.default;
